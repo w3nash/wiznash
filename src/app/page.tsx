be88f5621653afcd9d98d83967468bc61ec08238
Jonash Marcelino <thewwwnash@gmail.com>
2025-03-11 03:19:42 +0800

    Add theme provider and toggle components for dark/light mode support

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { ThemeToggle } from "@/components/theme-toggle";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
       <Button>Click me</Button>
+      <ThemeToggle className />
     </div>
   );
 }
